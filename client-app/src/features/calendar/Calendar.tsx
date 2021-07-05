0ff5a246f5435c8b0d108d1c918132603fe273a0
rheyan keneth <rpahuyo@gmail.com>
2021-07-05 14:34:04 +0800

    Added Calendar

--- client-app/src/features/calendar/Calendar.tsx
+++ client-app/src/features/calendar/Calendar.tsx
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Grid, Segment } from 'semantic-ui-react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 
 const Calendar = () => {
     return (
       <Grid>
-        <Grid.Column width={16} style={{width: '500px'}}>
-          <FullCalendar plugins={[dayGridPlugin]} initialView="dayGridMonth" />
+        <Grid.Column width={16}>
+          <FullCalendar 
+            plugins={[dayGridPlugin]} 
+            initialView="dayGridMonth" 
+            events={[
+              { title: 'Juan Dela Cruz', date: '2021-04-01' },
+              { title: 'Kim Jung eun', date: '2021-04-02' }
+            ]}/>
         </Grid.Column>
       </Grid>
     );
