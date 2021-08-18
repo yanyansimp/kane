54d4f32a12f78d19bb7d9f7e877d72c8f550bd06
rheyan keneth <rpahuyo@gmail.com>
2021-08-18 12:46:37 +0800

    Changed Id to Guid

--- Domain/TransactionType.cs
+++ Domain/TransactionType.cs
@@ -4,7 +4,7 @@ namespace Domain
 {
     public class TransactionType
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
