394d03e9e9be4384ffad954a5d2e1949a50afd57
rheyan keneth <rpahuyo@gmail.com>
2021-08-24 16:35:11 +0800

    Added Transaction

---
+++ API/Controllers/TransactionsController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Application.Transactions;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class TransactionsController : BaseController
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<ActionResult<Unit>> Create(Create.Command command)
+        {
+            return await Mediator.Send(command);
+        }
+    }
+}
