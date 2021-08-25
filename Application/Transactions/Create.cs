5b65988abf808f2e266edc68cf929fe63c994b22
rheyan keneth <rpahuyo@gmail.com>
2021-08-25 15:46:35 +0800

    Added Transaction functionalities

--- Application/Transactions/Create.cs
+++ Application/Transactions/Create.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Errors;
 using Application.Interfaces;
 using Domain;
 using FluentValidation;
@@ -37,19 +39,33 @@ namespace Application.Transactions
         public class Handler : IRequestHandler<Command>
         {
             private readonly DataContext _context;
-            public Handler(DataContext context)
+            private readonly IUserAccessor _userAccessor;
+            public Handler(DataContext context, IUserAccessor userAccessor)
             {
+                _userAccessor = userAccessor;
                 _context = context;
             }
 
             public async Task<Unit> Handle(Command request,
                     CancellationToken cancellationToken)
             {
-                var propertyType = await _context.PropertyTypes.FindAsync(request.PropertyTypeId);
+                // var propertyType = await _context.PropertyTypes.FindAsync(request.PropertyTypeId);
 
+                // Check if Property Exists
                 var property = await _context.Properties.FindAsync(request.PropertyId);
 
+                if (property == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { Property = "Not Found" });
+
+                // Check if Client Exists
                 var client = await _context.Clients.FindAsync(request.ClientId);
+
+                if (client == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { Client = "Not Found" });
+
+                // Gets the current User
+                var user = await _context.Users.SingleOrDefaultAsync(x => 
+                    x.UserName == _userAccessor.GetCurrentUsername());
 
                 var transaction = new Transaction
                 {
@@ -57,20 +73,28 @@ namespace Application.Transactions
                     ContractPrice = request.Transaction.ContractPrice,
                     MonthlyAmortization = request.Transaction.MonthlyAmortization,
                     Terms = request.Transaction.Terms,
-                    Status = "Ongoing",
+                    Status = "On Going",
                     Property = property,
                     SalesManagerId = request.SalesManagerId,
                     SalesAgentId = request.SalesAgentId,
                     CreatedAt = DateTime.Now
                 };
 
-                _context.Transactions.Add(transaction);
+                property.Status = property.Status == "Available" ? "Reserved" : property.Status;
+                
+                //
+                client.Transactions.Add(transaction);
+
+                //
+                user.Transactions.Add(transaction);
+
+                // _context.Transactions.Add(transaction);
 
                 var success = await _context.SaveChangesAsync() > 0;
 
-                if (success) 
+                if (success)
                     return Unit.Value;
-                    
+
                 throw new Exception("Problem saving changes");
 
             }
